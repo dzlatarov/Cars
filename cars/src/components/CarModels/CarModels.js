aac07824549a975d4117dd94f53e303b3541acf8
Danail Zlatarov <danzlatarov1@gmail.com>
2021-05-17 15:01:59 +0300

    render models

--- cars/src/components/CarModels/CarModels.js
+++ cars/src/components/CarModels/CarModels.js
@@ -3,32 +3,25 @@ import { useSelector } from 'react-redux'
 import './CarModels.css'
 
 const CarModels = () => {
-    const carsObject = useSelector(state => state.cars[0]);
-    const cars = carsObject ? Object.values(carsObject) : null;
-    const renderCars = () => {
-        if (cars) {
-            return cars.map(car => {
-                return (
-                    <li className="car-model__item" key={car.Model_ID}>
-                        <div className="car-model__content">
-                            <div className="car-model__header">
-                                Manufacturer: {car.Make_Name}
-                            </div>
-                            <div className="car-model__description">
-                                <p>Model: {car.Model_Name}</p>
-                            </div>
-                        </div>
-                    </li>
-                )
-            })
-        }
-
-        return null;
-    };
+    const models = useSelector(state => state.cars.models);       
 
     return (
         <ul className="car-models__items">
-            {renderCars()}
+            {models.length > 0 && (
+                models.map(model => (
+                    <li key={model.Model_ID}>
+                        <div>
+                            Manufacturer: {model.Make_Name}
+                        </div>
+                        <div>
+                            Model: {model.Model_Name}
+                        </div>
+                        <div>
+                            Type: {model.VehicleTypeName}
+                        </div>
+                    </li>
+                ))
+            )}
         </ul>
     );
 };
